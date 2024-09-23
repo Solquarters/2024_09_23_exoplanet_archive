9d0539959848f6526e208fa31282becfb1a998d0
Solquarters <rom.kabucov@gmail.com>
2024-09-23 23:05:17 +0200

    first commit exoplanet archive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { PlanetCardComponent } from './planet-card/planet-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, PlanetCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'basics';
+
+
+
+  buttonClicked(){
+    alert('Button clicked yo');
+  }
 }
